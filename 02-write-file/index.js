6a32cfbbbebffb30dd030f9e2b67b956856e327f
Alina Lisovets <alinalisovets699@gmail.com>
2025-01-22 19:17:42 +0300

    feat: add type to json file and fix 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-console.log('Здравствуйте! Введите текст: ');
+console.log('Здравствуйте! Введите текст. Для выхода нажмите ctrl + c или введите exit:  ');
 
 rl.on('line', (input) => {
   if (input === 'exit') {
